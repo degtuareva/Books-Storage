f474b7b749e9a320fa195fe2fbad4bf17e87d3af
olga.olegovna.603 <olga.olegovna.603@list.ru>
2025-08-20 22:01:24 +0300

    Добавление метода add

--- repo/java/spring-boot (1)/spring-boot/src/main/java/com/greatbit/BooksController.java
+++ repo/java/spring-boot (1)/spring-boot/src/main/java/com/greatbit/BooksController.java
@@ -1,9 +1,15 @@
 package com.greatbit;
 
+import com.greatbit.models.Book;
 import com.greatbit.models.BooksStorage;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.UUID;
 
 @Controller
 public class BooksController {
@@ -12,5 +18,17 @@ public class BooksController {
     public String booksList(Model model) {
         model.addAttribute("books", BooksStorage.getBooks());
         return "books-list";
+    }
+
+    @GetMapping("/create-form")
+    public String createForm() {
+        return "create-form";
+    }
+
+    @PostMapping("/create")
+    public String create(Book book) {
+        book.setId(UUID.randomUUID().toString());
+        BooksStorage.getBooks().add(book);
+        return "redirect:/";
     }
 }

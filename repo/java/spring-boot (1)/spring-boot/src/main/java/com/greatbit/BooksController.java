bce07a306a6636c072b44394a1063334be255207
olga.olegovna.603 <olga.olegovna.603@list.ru>
2025-08-21 00:42:33 +0300

    Добавление функционала update и delete

--- repo/java/spring-boot (1)/spring-boot/src/main/java/com/greatbit/BooksController.java
+++ repo/java/spring-boot (1)/spring-boot/src/main/java/com/greatbit/BooksController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -31,4 +32,42 @@ public class BooksController {
         BooksStorage.getBooks().add(book);
         return "redirect:/";
     }
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable("id") String id) {
+        Book bookToDelete=BooksStorage.getBooks().stream().
+                filter(b -> b.getId().equals(id)).
+                findFirst().
+                orElseThrow(RuntimeException::new);
+        BooksStorage.getBooks().remove(bookToDelete);
+        return "redirect:/";
+    }
+    @GetMapping("/edit-form/{id}")
+    public String editForm(@PathVariable("id") String id,
+                           Model model) {
+        Book bookToEdit=BooksStorage.getBooks().stream().
+                filter(b -> b.getId().equals(id)).
+                findFirst().
+                orElseThrow(RuntimeException::new);
+        model.addAttribute("book", bookToEdit);
+        return "edit-form";
+    }
+//    @PostMapping("/update")
+//    public String update(Book book) {
+//        delete(book.getId());
+//        BooksStorage.getBooks().add(book);
+//        return "redirect:/";
+//    }
+@PostMapping("/update")
+public String update(@ModelAttribute Book book) {
+    BooksStorage.getBooks().removeIf(b -> b.getId().equals(book.getId()));
+    BooksStorage.getBooks().add(book);
+    return "redirect:/";
+}
+
+    @PostMapping("/delete/{id}")
+    public String deleteBook(@PathVariable String id) {
+        BooksStorage.getBooks().removeIf(book -> book.getId().equals(id));
+        return "redirect:/";
+    }
+
 }
